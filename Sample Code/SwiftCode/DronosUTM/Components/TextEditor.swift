3389c6892633c740eb9a5f5e001fc49adc36e6f6
zvhir <zvhir@zvhirs-MacBook-Pro.local>
2023-07-13 11:48:34 +0800

    add icon, revamp project name, revamp folder sequence

---
+++ Sample Code/SwiftCode/DronosUTM/Components/TextEditor.swift
@@ -0,0 +1,21 @@
+//
+//  TextEditor.swift
+//  DronosUTM
+//
+//  Created by zvhir on 12/07/2023.
+//  Copyright © 2023 DJI. All rights reserved.
+//
+
+import SwiftUI
+
+struct TextEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        TextEditor()
+    }
+}
---
+++ Sample Code/SwiftCode/DronosUTM/Splashscreen/Splashscreen.swift
@@ -0,0 +1,15 @@
+//
+//  Splashscreen.swift
+//  DronosUTM
+//
+//  Created by zvhir on 12/07/2023.
+//  Copyright © 2023 DJI. All rights reserved.
+//
+
+import SwiftUI
+
+struct Splashscreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
