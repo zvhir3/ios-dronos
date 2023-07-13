63dfcd84e04eb0c2ae132df653d26c5d86687918
nadhirahh <nadhirahh97@gmail.com>
2023-07-13 17:30:13 +0800

    added splash screen

--- DronosUTM/Splashscreen/Splashscreen.swift
+++ DronosUTM/Splashscreen/Splashscreen.swift
@@ -8,8 +8,48 @@
 
 import SwiftUI
 
+//struct Splashscreen: View {
+//    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//    }
+//}
+
 struct Splashscreen: View {
+    @State private var logoIsVisible = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            if logoIsVisible {
+                LogoView()
+                    .transition(.move(edge: .bottom))
+                    .animation(.easeInOut(duration: 1.5))
+            }
+        }
+        .onAppear {
+            withAnimation {
+                logoIsVisible = true
+            }
+        }
+    }
+}
+
+
+struct LogoView: View {
+    @State private var offsetY: CGFloat = 400
+    
+    var body: some View {
+        Image("aerodyne")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 100, height: 100)
+            .offset(y: offsetY)
+            .onAppear {
+                withAnimation(.easeInOut(duration: 1.5)) {
+                    offsetY = 0
+                }
+            }
     }
 }
