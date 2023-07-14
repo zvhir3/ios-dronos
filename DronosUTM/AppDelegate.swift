1413b494fa5e5ee0a033d133317cf164fae6e6c0
nadhirahh <nadhirahh97@gmail.com>
2023-07-14 09:37:39 +0800

    added splash screen

--- DronosUTM/AppDelegate.swift
+++ DronosUTM/AppDelegate.swift
@@ -2,43 +2,12 @@ import UIKit
 import SwiftUI
 
 @main
-//class AppDelegate: UIResponder, UIApplicationDelegate {
-//
-//    var window: UIWindow?
-//
-//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//
-//        // Start the registration at the launch of the app. This can be retriggered at any time from the main view.
-//        // DJI App key needs to be registered in the Info.plist before calling this method.
-//        ProductCommunicationService.shared.registerWithProduct()
-//
-//        // Create the SwiftUI view
-//        let loginView = LoginPage()
-//
-//        // Use the SwiftUI view as the root view controller
-//        if #available(iOS 14.0, *) {
-//            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
-//                let window = UIWindow(windowScene: windowScene)
-//                window.rootViewController = UIHostingController(rootView: loginView)
-//                self.window = window
-//                window.makeKeyAndVisible()
-//            }
-//        } else {
-//            let window = UIWindow(frame: UIScreen.main.bounds)
-//            window.rootViewController = UIHostingController(rootView: loginView)
-//            self.window = window
-//            window.makeKeyAndVisible()
-//        }
-//
-//        return true
-//    }
-//}
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+
         // Start the registration at the launch of the app. This can be retriggered at any time from the main view.
         // DJI App key needs to be registered in the Info.plist before calling this method.
         ProductCommunicationService.shared.registerWithProduct()
@@ -55,15 +24,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if #available(iOS 14.0, *) {
             if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
                 let window = UIWindow(windowScene: windowScene)
-                window.rootViewController = UIHostingController(rootView: rootView)
+                let splashScreen = Splashscreen() // Replace with your actual splash screen view
+                let splashViewController = UIHostingController(rootView: splashScreen)
+                window.rootViewController = splashViewController
                 self.window = window
                 window.makeKeyAndVisible()
+
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    // After a delay of 3 seconds (or any desired duration), switch to the appropriate root view
+                    window.rootViewController = UIHostingController(rootView: rootView)
+                }
             }
         } else {
             let window = UIWindow(frame: UIScreen.main.bounds)
-            window.rootViewController = UIHostingController(rootView: rootView)
+            let splashScreen = Splashscreen() // Replace with your actual splash screen view
+            let splashViewController = UIHostingController(rootView: splashScreen)
+            window.rootViewController = splashViewController
             self.window = window
             window.makeKeyAndVisible()
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                // After a delay of 3 seconds (or any desired duration), switch to the appropriate root view
+                window.rootViewController = UIHostingController(rootView: rootView)
+            }
         }
         
         return true
