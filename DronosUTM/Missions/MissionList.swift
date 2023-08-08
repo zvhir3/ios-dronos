0ad20596db0d322ef79895fc7c4dfeb11d52caff
nadhirah-aeroydne <nadhirahh97@gmail.com>
2023-08-08 15:45:11 +0800

    [sc-8767] cater if mission is empty

--- DronosUTM/Missions/MissionList.swift
+++ DronosUTM/Missions/MissionList.swift
@@ -1,166 +1,8 @@
-//import UIKit
-//
-//class Missions: UIViewController {
-//
-//    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-//        return .portrait
-//    }
-//
-//    private let stackView: UIStackView = {
-//        let stackView = UIStackView()
-//        stackView.axis = .vertical
-//        stackView.alignment = .fill
-//        stackView.distribution = .equalSpacing
-//        stackView.spacing = 20
-//        stackView.translatesAutoresizingMaskIntoConstraints = false
-//        return stackView
-//    }()
-//
-//    private let scrollView: UIScrollView = {
-//        let scrollView = UIScrollView()
-//        scrollView.translatesAutoresizingMaskIntoConstraints = false
-//        return scrollView
-//    }()
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        setupUI()
-//        fetchMissions()
-//    }
-//
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//
-//        title = "Missions"
-//
-//        let appearance = UINavigationBarAppearance()
-//        appearance.backgroundColor = UIColor.fromHex(0x1e293b)
-//        UINavigationBar.appearance().standardAppearance = appearance
-//        UINavigationBar.appearance().scrollEdgeAppearance = appearance
-//        UINavigationBar.appearance().backIndicatorImage = UIImage()
-//        UINavigationBar.appearance().backIndicatorTransitionMaskImage = UIImage()
-//
-//        let bottomSafeAreaView = UIView()
-//        bottomSafeAreaView.backgroundColor = UIColor.red
-//
-//        view.addSubview(bottomSafeAreaView)
-//        bottomSafeAreaView.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//            bottomSafeAreaView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-//            bottomSafeAreaView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-//            bottomSafeAreaView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-//            bottomSafeAreaView.topAnchor.constraint(equalTo: view.bottomAnchor)
-//        ])
-//    }
-//
-//    private func fetchMissions() {
-//        APIService.fetchMissions { [weak self] missions in
-//            DispatchQueue.main.async {
-//                for mission in missions {
-//                    let cardView = self?.createCardView(mission: mission)
-//                    if let cardView = cardView {
-//                        self?.stackView.addArrangedSubview(cardView)
-//                        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self?.cardTapped(_:)))
-//                        cardView.addGestureRecognizer(tapGestureRecognizer)
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//    private func setupUI() {
-//        view.backgroundColor = UIColor.fromHex(0x14181f)
-////        view.backgroundColor = UIColor(red: 34/255.0, green: 38/255.0, blue: 50/255.0, alpha: 0.8)
-//
-//        let titleLabel = UILabel.makeTitleLabel(text: "Mission List")
-////        let connectedDroneLabel = UILabel.makeConnectedDroneLabel(text: "Connected Drone: -")
-//
-//        view.addSubview(scrollView)
-////        scrollView.addSubview(connectedDroneLabel)
-//        scrollView.addSubview(titleLabel)
-//        scrollView.addSubview(stackView)
-//
-//        NSLayoutConstraint.activate([
-//            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-//            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-//            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-//            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-//            titleLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 16),
-//            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-//            stackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
-//            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
-//            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
-//            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -16),
-//            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -32)
-//        ])
-//    }
-//
-//
-//    private func createCardView(mission: APIService.Mission) -> UIView {
-//
-//        let scheduleText = mission.schedules.map { schedule in
-//            return "Start Date: \(schedule.startDate)\(schedule.startTime) End Date: \(schedule.endDate) \(schedule.endTime)"
-//        }.joined(separator: "\n")
-//
-//        let coordinateText = mission.area.coordinate.map { coordinate in
-//            return "Coordinate: \(coordinate.latitudeDouble),\(coordinate.longitudeDouble)"
-//        }.joined(separator: "")
-//
-//        let missionIdLabel = UILabel.makeLabel(text: "\(mission.missionId)", textColor: UIColor.fromHex(0xffffff))
-//        let nameLabel = UILabel.makeLabel(text: "Name: \(mission.name)", textColor: UIColor.fromHex(0xffffff))
-//        let locationLabel = UILabel.makeLabel(text: "Location: \(mission.location)", textColor: UIColor.fromHex(0xffffff))
-//        let scheduleLabel = UILabel.makeLabel(text: "\(scheduleText)", textColor: UIColor.fromHex(0xffffff))
-//        let coordinateLabel = UILabel.makeLabel(text: "Coordinate: \(coordinateText)", textColor: UIColor.fromHex(0xffffff))
-//
-//        coordinateLabel.translatesAutoresizingMaskIntoConstraints = false
-//        scheduleLabel.translatesAutoresizingMaskIntoConstraints = false
-//
-//        let cardView = UIView()
-//        cardView.backgroundColor = .lightGray
-//        cardView.layer.cornerRadius = 8
-//        cardView.translatesAutoresizingMaskIntoConstraints = false
-//        cardView.isUserInteractionEnabled = true
-//        cardView.backgroundColor = UIColor.fromHex(0x1e293b)
-//        cardView.layer.cornerRadius = 8
-//        cardView.layer.shadowColor = UIColor.fromHex(0x00f0ff).cgColor
-//        cardView.layer.shadowRadius = 2
-//        cardView.layer.shadowOpacity = 1
-//        cardView.layer.shadowOffset = CGSize(width: 0, height: 0)
-//        cardView.translatesAutoresizingMaskIntoConstraints = false
-//        cardView.isUserInteractionEnabled = true
-//        cardView.addSubview(missionIdLabel)
-//        cardView.addSubview(nameLabel)
-//        cardView.addSubview(locationLabel)
-//        cardView.addSubview(coordinateLabel)
-//        cardView.addSubview(scheduleLabel)
-//
-//        NSLayoutConstraint.activate([
-//            missionIdLabel.topAnchor.constraint(equalTo: cardView.topAnchor, constant: 16),
-//            missionIdLabel.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: 16),
-//            nameLabel.topAnchor.constraint(equalTo: missionIdLabel.bottomAnchor, constant: 8),
-//            nameLabel.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: 16),
-//            locationLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
-//            locationLabel.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: 16),
-//            coordinateLabel.topAnchor.constraint(equalTo: locationLabel.bottomAnchor, constant: 8),
-//            coordinateLabel.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: 16),
-//            scheduleLabel.topAnchor.constraint(equalTo: coordinateLabel.bottomAnchor, constant: 8),
-//            scheduleLabel.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: 16),
-//            scheduleLabel.bottomAnchor.constraint(equalTo: cardView.bottomAnchor, constant: -16)
-//        ])
-//
-//        return cardView
-//    }
-//
-//    @objc private func cardTapped(_ sender: UITapGestureRecognizer) {
-////        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-////        if let mainViewController = storyboard.instantiateInitialViewController() {
-////            present(mainViewController, animated: true, completion: nil)
-////        }
-//    }
-//}
-//
+
 
 import SwiftUI
+
+var isEmptyMission: Bool = false
 
 struct Mission: Identifiable {
     let id = UUID()
@@ -177,27 +19,27 @@ struct Mission: Identifiable {
     }
     
     var status: String {
-            let formatter = ISO8601DateFormatter()
-            
-            let startDate = formatter.date(from: schedules.first?.startDate ?? "") ?? Date()
-            let endDate = formatter.date(from: schedules.first?.endDate ?? "") ?? Date()
-            
-            let currentTime = Calendar.current.dateComponents([.hour, .minute], from: Date())
-            let startTime = Calendar.current.dateComponents([.hour, .minute], from: formatDateTime(schedules.first?.startTime ?? 0))
-            let endTime = Calendar.current.dateComponents([.hour, .minute], from: formatDateTime(schedules.first?.endTime ?? 0))
-            
-            let today = Date()
-
-            if today > endDate {
-                return "Completed"
-            } else if today < startDate {
-                return "Scheduled"
-            } else if today < endDate && today > startDate && currentTime.hour! > startTime.hour! && currentTime.minute! > startTime.minute! && currentTime.hour! < endTime.hour! && currentTime.minute! < endTime.minute! {
-                return "Active"
-            } else {
-                return "Unknown"
-            }
+        let formatter = ISO8601DateFormatter()
+        
+        let startDate = formatter.date(from: schedules.first?.startDate ?? "") ?? Date()
+        let endDate = formatter.date(from: schedules.first?.endDate ?? "") ?? Date()
+        
+        let currentTime = Calendar.current.dateComponents([.hour, .minute], from: Date())
+        let startTime = Calendar.current.dateComponents([.hour, .minute], from: formatDateTime(schedules.first?.startTime ?? 0))
+        let endTime = Calendar.current.dateComponents([.hour, .minute], from: formatDateTime(schedules.first?.endTime ?? 0))
+        
+        let today = Date()
+        
+        if today > endDate {
+            return "Completed"
+        } else if today < startDate {
+            return "Scheduled"
+        } else if today < endDate && today > startDate && currentTime.hour! > startTime.hour! && currentTime.minute! > startTime.minute! && currentTime.hour! < endTime.hour! && currentTime.minute! < endTime.minute! {
+            return "Active"
+        } else {
+            return "Unknown"
         }
+    }
 }
 
 struct Schedule: Hashable {
@@ -225,6 +67,8 @@ struct Coordinate: Hashable {
 
 class MissionsViewModel: ObservableObject {
     @Published var missions: [Mission] = []
+//    @State private var isEmptyMission: Bool = false
+    
     
     
     
@@ -232,7 +76,14 @@ class MissionsViewModel: ObservableObject {
         
         APIService.fetchMissions { serviceMissions in
             DispatchQueue.main.async {
-                self.missions = serviceMissions.map(Mission.init(from:))
+                if !serviceMissions.isEmpty {
+                    self.missions = serviceMissions.map(Mission.init(from:))
+                    isEmptyMission = false
+                } else {
+                    // Handle empty array response
+                    print("The response array is empty")
+                    isEmptyMission = true
+                }
             }
         }
         
@@ -242,6 +93,9 @@ class MissionsViewModel: ObservableObject {
 struct MissionsView: View {
     @StateObject private var viewModel = MissionsViewModel()
     @State private var searchMission: String = ""
+    @State private var date = Date()
+    @State private var showingDatePicker: Bool = false
+//    @State private var isEmptyMission: Bool = false
     var body: some View {
         NavigationView {
             ZStack {
@@ -278,43 +132,50 @@ struct MissionsView: View {
                         .cornerRadius(10)
                         .padding(.horizontal, 5)
                         .padding(.vertical, 25)
-                        //
-                        //                        VStack(alignment: .center, spacing: 8) { }
-                        //                            .padding(0)
-                        //                            .frame(width: 50, height: 48, alignment: .center)
-                        //                            .background(.white.opacity(0.1))
-                        //                            .cornerRadius(4)
-                        //                            .shadow(color: .black.opacity(0.25), radius: 1, x: 1, y: 1)
-                        //                            .overlay(
-                        //                                ZStack {
-                        //                                    RoundedRectangle(cornerRadius: 4)
-                        //                                        .inset(by: 0.29)
-                        //                                        .stroke(.white.opacity(0.1), lineWidth: 0.58)
-                        //
-                        //                                    Image("filter-icon") // SF Symbol for a magnifying glass
-                        //                                        .resizable() // Add this
-                        //                                        .frame(width: 20, height: 20) // Specify the size you want here
-                        //                                        .foregroundColor(.white)
-                        //                                        .padding(.horizontal, 10)
-                        //
-                        //                                }
-                        //                            )
+                        
+                        VStack(alignment: .center, spacing: 8) {
+                            
+                        }
+                        .padding(0)
+                        .frame(width: 50, height: 48, alignment: .center)
+                        .background(.white.opacity(0.1))
+                        .cornerRadius(4)
+                        .shadow(color: .black.opacity(0.25), radius: 1, x: 1, y: 1)
+                        .overlay(
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 4)
+                                    .inset(by: 0.29)
+                                    .stroke(.white.opacity(0.1), lineWidth: 0.58)
+                                
+                                Image("filter-icon") // SF Symbol for a magnifying glass
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                    .foregroundColor(.white)
+                                    .padding(.horizontal, 10)
+                            }
+                        )
                     }
                     
                     .padding(.horizontal, 24)
                     .padding(.vertical, 0)
                     .frame(maxWidth: .infinity, alignment: .top)
-                    
-                    //                    NavigationView {
                     ScrollView {
-                        ForEach(viewModel.missions) { mission in
-                            NavigationLink(destination: Launchpad().transition(.move(edge: .trailing))
-                                .animation(.easeInOut(duration: 0.5), value: true)) {
-                                    ZStack(alignment: .top) {
-                                        
-                                        MissionCardView(mission: mission)
-                                            .padding(.bottom, 10)
-                                        VStack(spacing: -32) {
+                        
+                            
+                        if viewModel.missions.isEmpty {
+                            Text("No Mission available")
+                                .italic()
+                                .font(.system(size: 22))
+                                .foregroundColor(Color.white)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                        } else {
+                            ForEach(viewModel.missions) { mission in
+                                NavigationLink(destination: Launchpad().transition(.move(edge: .trailing))
+                                    .animation(.easeInOut(duration: 0.5), value: true)) {
+                                        ZStack(alignment: .top) {
+                                            MissionCardView(mission: mission)
+                                                .padding(.bottom, 10)
+                                            VStack(spacing: -32) {
                                                 HStack {
                                                     Spacer().frame(width: 340 / 1.19)
                                                     HStack {
@@ -332,21 +193,18 @@ struct MissionsView: View {
                                                     .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
                                                 }
                                                 .padding(.top, 10)
-                                            HStack {
-                                                Spacer().frame(width: 345)
-                                                // Add the image here
-                                                Image("status-img")
-                                                    .resizable() // Add this if your image is not appearing
+                                                HStack {
+                                                    Spacer().frame(width: 345)
+                                                    // Add the image here
+                                                    Image("status-img")
+                                                        .resizable() // Add this if your image is not appearing
+                                                }
                                             }
                                         }
-
-                                       
-                                        
                                     }
-                                   
-                                    
-                                }
+                            }
                         }
+
                     }
                     //                        }
                 }
@@ -362,9 +220,9 @@ struct MissionsView: View {
     }
 }
 
+
 struct MissionCardView: View {
     let mission: Mission
-    
     var body: some View {
         //        ZStack {
         VStack(alignment: .leading, spacing: 8) {
@@ -377,7 +235,7 @@ struct MissionCardView: View {
                     .resizable() // Add this
                     .frame(width: 5, height: 5)
                 
-                Text("MSN 54048 9384E ")
+                Text(mission.missionId)
                     .font(.system(size: 10))
                     .foregroundColor(Color(red: 0.78, green: 0.78, blue: 0.78))
             }
@@ -404,8 +262,8 @@ struct MissionCardView: View {
     }
 }
 
-struct MissionsView_Previews: PreviewProvider {
-    static var previews: some View {
-        MissionsView()
-    }
-}
+//struct MissionsView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MissionsView()
+//    }
+//}
